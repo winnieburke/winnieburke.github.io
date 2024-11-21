b605753b2d674a47774c672b1327ef04ea3f33f0
winnieburke <winniemayburke@gmail.com>
2024-11-21 10:11:53 -0700

    js game in progress

---
+++ JS_Game/game.js
@@ -0,0 +1,9 @@
+// Objective of game!
+// You play as a cat trying to find your lost toys
+
+//Level 1 : Backyard search 
+//You look through randomly spawnning bushes
+//Using rand I will generate a random amount of bushes and a seperate function will play the lost toy in one of the generated bushes
+
+
+//Level 2 : You travel past the fence of the backyard into the wild
